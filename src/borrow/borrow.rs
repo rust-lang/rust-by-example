2c4328574e311ad97c08779cf5abfe351de4dadd
J. Cliff Dyer <jcd@sdf.lonestar.org>
2014-04-26 11:36:13 -0400

    Added a bit about the effect of borrows on the owning variables.

--- src/borrow/borrow.rs
+++ src/borrow/borrow.rs
@@ -45,6 +45,26 @@ fn main() {
     // Ok: mutable objects can be immutably borrowed
     borrow_book(&mutable_geb);
 
+    // when an object is mutably borrowed, the original can't be used
+    // until the end of the borrowed copy's lifetime
+    if true {
+        let borrowed_geb = &mut mutable_geb;
+
+        // Error: mutable_geb has been mutably borrowed.
+        //println!("Can no longer access {}", mutable_geb.title);
+
+        println!("The borrowed copy of {} is available", borrowed_geb.title);
+ 	    // now borrowed_geb goes out of scope
+    }
+    println!("Once again, I can access {}", mutable_geb.title);
+    
+    // immutable borrows place no restrictions on the original owner
+    if true {
+        let borrowed_geb = &mutable_geb;
+        println!("The original is still accessible: {}", mutable_geb.title);
+        println!("and so is the borrowed copy: {}", borrowed_geb.title);
+    }
+
     // ref can be used to take references when destructuring
     // Here geb_title and geb_author are references to ~str, i.e. &~str
     // geb_year is a copy of the year field
