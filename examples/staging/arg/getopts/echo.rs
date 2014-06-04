0eb6bfe700856fb8731e9a4311b6d036c30606d5
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2014-06-04 16:42:13 -0400

    Add chapter on command-line arguments

---
+++ examples/staging/arg/getopts/echo.rs
@@ -0,0 +1,64 @@
+extern crate getopts;
+
+use std::os;
+use std::io::{print, println};
+use std::io::stdio;
+
+static VERSION: &'static str = "1.0.0";
+
+fn main() {
+    let args = os::args();
+    let ref program = args[0];
+
+    // Set possible flags.
+    // The first argument to `optflag` is the short flag name.
+    // The second argument is the long flag name.
+    // The third argument is the help text.
+    let opts = [
+        getopts::optflag("n", "", "do not output the trailing newline"),
+        getopts::optflag("h", "help", "display this help and exit"),
+        getopts::optflag("V", "version",
+                         "output version information and exit"),
+    ];
+
+    let matches = match getopts::getopts(args.tail(), &opts) {
+        Ok(m) => m,
+        Err(f) => {
+            println!("{}", f);
+            os::set_exit_status(1);
+            return;
+            // The exit code is 0 (success) by default.
+            // Any exit code other than 0 indicates failure.
+        }
+    };
+
+    if matches.opt_present("help") {
+        //^ We could as well have used the short name: "h"
+        println!("echo {} - display a line of text", VERSION);
+        println!("");
+        println!("Usage:");
+        println!(" {} [SHORT-OPTION]... [STRING]...", program);
+        println!(" {} LONG-OPTION", program);
+        println!("");
+        println(getopts::usage("Echo the STRING(s) to standard output.", &opts)
+                .as_slice());
+        return;
+    }
+
+    if matches.opt_present("version") {
+        println!("echo version: {}", VERSION);
+        return;
+    }
+
+    if !matches.free.is_empty() {
+        //^ `matches.free` contains all the arguments that are not options.
+        let string = matches.free.connect(" ");
+        print(string.as_slice());
+    }
+
+    if !matches.opt_present("n") {
+        println!("")
+    } else {
+        stdio::flush();
+    }
+}
---
+++ examples/staging/arg/getopts/testopt.rs
@@ -0,0 +1,38 @@
+extern crate getopts;
+
+use std::os;
+
+fn main() {
+    let args = os::args();
+    let opts = [
+        getopts::optflag("a", "long_a", ""),
+        getopts::optflag("b", "long_b", ""),
+        getopts::optopt("c", "long_c", "", "VALUE"),
+        //^ Use `optflagopt` if the argument should be optional.
+        //  Use `reqopt` if the option is required.
+        //  Use `optmulti`, `optflagmulti` if options can occur multiple times.
+    ];
+
+    let matches = match getopts::getopts(args.tail(), &opts) {
+        Ok(m) => m,
+        Err(f) => {
+            println!("{}", f);
+            os::set_exit_status(1);
+            return;
+        }
+    };
+    let a = if matches.opt_present("a") {true} else {false};
+    let b = if matches.opt_present("b") {true} else {false};
+    let c = match matches.opt_str("c") {
+        Some(s) => s,
+        None => String::from_str(""),
+    };
+    //^ Use `matches.opt_default` if you need a default (`opflagopt`).
+    //  Use `matches.opt_count` if you need to count how many were matched
+    //  (`*multi`).
+
+    println!("a={}, b={}, c=\"{}\"", a, b, c);
+    if !matches.free.is_empty() {
+        println!("free arguments: {}", matches.free);
+    }
+}
