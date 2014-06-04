c3d4c7338c345113e1df747e13322ed4013662a0
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2014-06-04 16:42:13 -0400

    Add chapter on commandline arguments

---
+++ examples/arg/args.rs
@@ -0,0 +1,7 @@
+use std::os;
+
+fn main() {
+    let args = os::args();
+    println!("My path is {}.", args.get(0));
+    println!("My arguments are {}.", args.slice(1, args.len()));
+}
---
+++ examples/arg/input.md
@@ -0,0 +1,8 @@
+The command line arguments can be accessed using `std::os::args`:
+
+{args.rs}
+
+{args.play}
+
+{args.out}
+
