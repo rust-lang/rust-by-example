3b8e3a8d5fa670468758770f198b060aecadc9d4
Jorge Aparicio <japaric@linux.com>
2014-06-11 10:14:46 -0500

    constants: fix style, add FIXME

--- examples/constants/constants.rs
+++ examples/constants/constants.rs
@@ -2,26 +2,27 @@ static LANGUAGE: &'static str = "Rust";
 static THRESHOLD: int = 10;
 
 fn is_big(n: int) -> bool {
-    // access constant in some function
+    // Access constant in some function
     n > THRESHOLD
 }
 
 fn main() {
     let n = 16;
 
-    // access constant in the main loop
+    // Access constant in the main task
     println!("This is {}", LANGUAGE);
     println!("The threshold is {}", THRESHOLD);
     println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
 
-    // Error: cannot modify static item
-    //THRESHOLD = 5;
+    // Error! Cannot modify an static item
+    THRESHOLD = 5;
+    // FIXME ^ Comment out this line
 
-    if true {
-        // string literals are references to read-only memory
-        let static_string: &'static str = "In read-only memory";
+    {
+        // String literals are references to read-only memory
+        let _static_string: &'static str = "In read-only memory";
 
-        // when static_string goes out of scope, we can no longer refer to the
-        // underlying data, but the string remains in the read-only memory
+        // When `_static_string` goes out of scope, we can no longer refer to
+        // the underlying data, but the string remains in the read-only memory
     }
 }
