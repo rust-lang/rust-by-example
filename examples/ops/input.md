1907a5735a623bb77968020d8ef941837936a809
Jorge Aparicio <japaric@linux.com>
2014-06-11 16:04:35 -0500

    operator: rename to ops, fix style

--- examples/ops/input.md
+++ examples/ops/input.md
@@ -1,6 +1,6 @@
 In Rust, many of the operators can be overloaded via traits. This is possible
 because operators are just sugar for method calls. For example, `a + b`
-desugars to `a.add(&b)`. This `add()` method is part of the `Add` trait, hence
+desugars to `a.add(&b)`. This `add` method is part of the `Add` trait, hence
 any implementor of the `Add` trait will be able to use the `+` operator.
 
 {operator.play}
--- examples/ops/operator.rs
+++ examples/ops/operator.rs
@@ -7,12 +7,12 @@ struct FooBar;
 #[deriving(Show)]
 struct BarFoo;
 
-// the Add<T, U> trait needs two generic parameters:
-// T is the type of the RHS summand, and
-// U is the type of the sum
+// The `Add<T, U>` trait needs two generic parameters:
+// * T is the type of the RHS summand, and
+// * U is the type of the sum
 // This block implements the operation: Foo + Bar = FooBar
 impl Add<Bar, FooBar> for Foo {
-    fn add(&self, rhs: &Bar) -> FooBar {
+    fn add(&self, _rhs: &Bar) -> FooBar {
         println!("> Foo.add(&Bar) was called");
 
         FooBar
@@ -22,7 +22,7 @@ impl Add<Bar, FooBar> for Foo {
 // Addition can be implemented in a non-commutative way
 // This block implements the operation: Bar + Foo = BarFoo
 impl Add<Foo, BarFoo> for Bar {
-    fn add(&self, rhs: &Foo) -> BarFoo {
+    fn add(&self, _rhs: &Foo) -> BarFoo {
         println!("> Bar.add(&Foo) was called");
 
         BarFoo
