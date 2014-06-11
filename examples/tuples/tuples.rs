7ca724de59704996f1cb26fe4edbd32d52fa5894
Jorge Aparicio <japaric@linux.com>
2014-06-10 20:31:22 -0500

    tuples: fix style

--- examples/tuples/tuples.rs
+++ examples/tuples/tuples.rs
@@ -1,4 +1,4 @@
-// tuples can be used as function arguments and as return values
+// Tuples can be used as function arguments and as return values
 fn reverse(pair: (int, bool)) -> (bool, int) {
     // `let` can be used to bind the members of a tuple to variables
     let (integer, boolean) = pair;
@@ -7,27 +7,29 @@ fn reverse(pair: (int, bool)) -> (bool, int) {
 }
 
 fn main() {
-    // a tuple with a bunch of different types
+    // A tuple with a bunch of different types
     let long_tuple = (1u8, 2u16, 3u32, 4u64,
                       -1i8, -2i16, -3i32, -4i64,
                       0.1f32, 0.2f64,
                       'a', true);
 
-    // values can be extracted from the tuple using the valN methods
+    // Values can be extracted from the tuple using the `valN` methods
     println!("long tuple first value: {}", long_tuple.val0());
     println!("long tuple second value: {}", long_tuple.val1());
 
-    // tuples can be tuple members
+    // Tuples can be tuple members
     let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
 
-    // tuples are printable
+    // Tuples are printable
+    println!("tuple of tuples: {}", tuple_of_tuples);
+
     let pair = (1, true);
     println!("pair is {}", pair);
 
     println!("the reversed pair is {}", reverse(pair));
 
-    // to create one element tuples, the comma is required to tell them
-    // apart from a literal surrounded by parentheses
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses
     println!("one element tuple: {}", (5,))
     println!("just an integer: {}", (5))
 }
