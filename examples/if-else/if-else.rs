07c67ef611ab6ca73eafa62ac5d668b367bc6e96
Jorge Aparicio <japaric@linux.com>
2014-06-10 20:13:16 -0500

    if-else: merge source files in one

--- examples/if-else/if-else.rs
+++ examples/if-else/if-else.rs
@@ -2,10 +2,26 @@ fn main() {
     let n = 5;
 
     if n < 0 {
-        println!("{} is negative", n);
+        print!("{} is negative", n);
     } else if n > 0 {
-        println!("{} is positive", n);
+        print!("{} is positive", n);
     } else {
-        println!("{} is zero", n);
+        print!("{} is zero", n);
     }
+
+    let big_n =
+        if n < 10 && n > -10 {
+            println!(", and is a small number, increase ten-fold");
+
+            // This expression returns an `int`
+            10 * n
+        } else {
+            println!(", and is a big number, reduce by two");
+
+            // This expression must return an `int` as well
+            n / 2
+            // TODO ^ Try suppressing this expression with a semicolon
+        };
+
+    println!("{} -> {}", n, big_n);
 }
