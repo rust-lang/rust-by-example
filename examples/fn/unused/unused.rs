807757b78bbf5b11efe810f75f7118ea8d2a01d3
Jorge Aparicio <japaric@linux.com>
2014-06-12 15:15:21 -0500

    fn: add sub-section on unused functions

---
+++ examples/fn/unused/unused.rs
@@ -0,0 +1,12 @@
+fn used_function() {}
+
+// `#[allow(dead_code)]` is an attribute that disables the `dead_code` lint
+#[allow(dead_code)]
+fn unused_function() {}
+
+fn noisy_unused_function() {}
+// FIXME ^ Add an attribute to suppress the warning
+
+fn main() {
+    used_function();
+}
