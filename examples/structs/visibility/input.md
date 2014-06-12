63f5da2c44d08b31a3f2bb0059441c401977c0b3
Jorge Aparicio <japaric@linux.com>
2014-06-12 15:40:28 -0500

    structs: add visibility sub-section

---
+++ examples/structs/visibility/input.md
@@ -0,0 +1,6 @@
+Structs have an extra level of visibility, their fields can be public or
+private (which is the default). This visibility only matters when
+structs are accessed from outside the module where they are defined, and its
+goal is information hiding (encapsulation).
+
+{struct.play}
---
+++ examples/structs/visibility/struct.rs
@@ -0,0 +1,43 @@
+mod my {
+    // A public struct with public fields
+    pub struct WhiteBox<T> {
+        pub contents: T,
+    }
+
+    // A public struct with private fields
+    #[allow(dead_code)]
+    pub struct BlackBox<T> {
+        contents: T,
+    }
+
+    impl<T> BlackBox<T> {
+        // A public constructor
+        pub fn new(contents: T) -> BlackBox<T> {
+            BlackBox {
+                contents: contents,
+            }
+        }
+    }
+}
+
+fn main() {
+    // Public structs with public fields can be constructed as usual
+    let white_box = my::WhiteBox { contents: "public information" };
+
+    // and their fields can be normally accessed
+    println!("The white box contains: {}", white_box.contents);
+
+    // but public structs with private fields can't be constructed
+    // Error! `BlackBox` has private fields
+    //let black_box = my::BlackBox { contents: "classified information" };
+    // TODO ^ Try uncommenting this line
+
+    // However, structs with private fields can still be created using
+    // constructors
+    let _black_box = my::BlackBox::new("classified information");
+
+    // The private fields of a struct can't be accessed
+    // Error! The `contents` field is private
+    //println!("The black box contains: {}", black_box.contents);
+    // TODO ^ Try uncommenting this line
+}
