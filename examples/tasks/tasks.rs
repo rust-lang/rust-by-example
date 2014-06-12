7753487f4a50d171b68e7cca06add4f6cfb712c3
Jorge Aparicio <japaric@linux.com>
2014-06-11 22:16:37 -0500

    tasks: fix style

--- examples/tasks/tasks.rs
+++ examples/tasks/tasks.rs
@@ -2,7 +2,7 @@ static NTASKS: int = 10;
 
 fn main() {
     for i in range(0, NTASKS) {
-        // spin up a task
+        // Spin up a task
         spawn(proc() {
             println!("this is task number {}", i)
         });
