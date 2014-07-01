128761c9ac83362e04bdb50a1c398483b2ee6ea4
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2014-06-30 21:41:36 -0400

    echo.rs: a few minor improvements

--- examples/staging/arg/getopts/echo.rs
+++ examples/staging/arg/getopts/echo.rs
@@ -1,15 +1,14 @@
 extern crate getopts;
-extern crate libc;
 
 use std::os;
 use std::io::{print, println};
-use std::io::stdio::flush;
+use std::io::stdio;
 
 static VERSION: &'static str = "1.0.0";
 
 fn main() {
     let args = os::args();
-    let program = args.get(0).clone();
+    let program = args.get(0).as_slice();
 
     // Set possible flags.
     // The first argument to `optflag` is the short flag name.
@@ -34,11 +33,11 @@ fn main() {
 
     if matches.opt_present("help") {
         //^ We could as well have used the short name.
-        println!("echo {:s} - display a line of text", VERSION);
+        println!("echo {} - display a line of text", VERSION);
         println!("");
         println!("Usage:");
-        println!(" {0:s} [SHORT-OPTION]... [STRING]...", program);
-        println!(" {0:s} LONG-OPTION", program);
+        println!(" {} [SHORT-OPTION]... [STRING]...", program);
+        println!(" {} LONG-OPTION", program);
         println!("");
         println(getopts::usage("Echo the STRING(s) to standard output.", opts)
                 .as_slice());
@@ -51,7 +50,7 @@ fn main() {
     }
 
     if !matches.free.is_empty() {
-        //^ `matches.free` contains all arguments that are not options.
+        //^ `matches.free` contains all the arguments that are not options.
         let string = matches.free.connect(" ");
         print(string.as_slice());
     } 
@@ -59,6 +58,6 @@ fn main() {
     if !matches.opt_present("n") {
         println!("")
     } else {
-        flush();
+        stdio::flush();
     }
 }
