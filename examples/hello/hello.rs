424e73aeb3fdad5cbbbfd787735ed9c6ef4d3b79
NODA, Kai <nodakai@gmail.com>
2014-09-02 15:29:04 +0800

    hello: usage go into the text from the comments.
    Also briefly introduce other styles of comments.

--- examples/hello/hello.rs
+++ examples/hello/hello.rs
@@ -1,14 +1,13 @@
 // This is a comment, and will be ignored by the compiler
-// You can test this code by clicking the "Run" button over there ->
-// or if prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
 
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
+/*
+ There are also block style comments
+ /* which can be nested */
+*/
 
-// This is the main function
+/// This is the main function (three slashes are for `rustdoc`)
 fn main() {
     // The statements here will be executed when the compiled binary is called
 
-    // Print text to the console
-    println!("Hello World!");
+    println!("Hello World!"); // Print text to the console
 }
--- examples/hello/input.md
+++ examples/hello/input.md
@@ -1,5 +1,13 @@
 This is the source code of the traditional Hello World program.
 
+You can test this code by clicking the "Run" button on the top right corner.
+
+Or if prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut.
+
+The code is also editable.  Feel free to hack it!
+
+You can always return to the original code by clicking the "Reset" button.
+
 {hello.play}
 
 `println!` is a *macro* (we'll cover them later) that prints text to the
