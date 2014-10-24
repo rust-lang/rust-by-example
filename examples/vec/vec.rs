b02d5cb9bf96803dd7b0aa8c17d1dca2102f3aa6
Aman Orazaev <aorazaev@gmail.com>
2014-10-24 19:44:04 +0400

    No more Vec::get() method in rust

--- examples/vec/vec.rs
+++ examples/vec/vec.rs
@@ -19,12 +19,12 @@ fn main() {
     // The `len` method yields the current size of the vector
     println!("Vector size: {}", xs.len());
 
-    // Indexing is done using the `get` method (indexing starts at 0)
-    println!("Second element: {}", xs.get(1));
+    // Indexing is done using the square brackets (indexing starts at 0)
+    println!("Second element: {}", xs[1]);
 
     // `pop` removes the last element from the vector and returns it
     println!("Pop last element: {}", xs.pop());
 
     // Out of bounds indexing yields a task failure
-    println!("Fourth element: {}", xs.get(3));
+    println!("Fourth element: {}", xs[3]);
 }
