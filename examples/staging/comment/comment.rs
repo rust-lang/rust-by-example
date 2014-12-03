92765ce59e80c5f4c3c1498ff807a38603e29ef8
Nicholas Mazzuca <npmazzuca@gmail.com>
2014-12-02 22:10:30 -0800

    Added comments to staging

---
+++ examples/staging/comment/comment.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // This is an example of a line comment
+    // Notice how there are two slashes at the beginning of the line
+    // And that anything written inside these will not be read by the compiler
+
+    // println!("Hello, world!");
+
+    // Run it. See? Now try deleting the two slashes, and run it again.
+    
+    /* 
+     * This is another type of comment, the block comment. It's not used 
+     * in Rust very often, and is against the Rust Style Guide.
+     */
+}
