175b358fb4a17b91f3521657b161d46be8fd5e7c
Steve Klabnik <steve@steveklabnik.com>
2014-12-08 13:48:30 -0500

    Fix the build

--- examples/staging/unsafe/transmute.rs
+++ examples/staging/unsafe/transmute.rs
@@ -2,6 +2,6 @@ fn main() {
     let u: &[u8] = &[49, 50, 51];
 
     unsafe {
-        assert!(u == std::mem::transmute("123"));
+        assert!(u == std::mem::transmute::<&str, &[u8]>("123"));
     }
 }
