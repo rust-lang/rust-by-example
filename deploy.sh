9b4013b9954cb299742be51fd887e68b34e5ffa9
Steve Klabnik <steve@steveklabnik.com>
2014-12-13 08:31:22 -0500

    Don't reset so --hard
    This was trashing the local copy, which means that we erased our generated files.
    
    Fixes #318

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ git init
 git config user.name "Steve Klabnik"
 git config user.email "steve@steveklabnik.com"
 git remote add upstream "https://$GH_TOKEN@github.com/rust-lang/rust-by-example.git"
-git fetch upstream && git reset --hard upstream/gh-pages
+git fetch upstream && git reset upstream/gh-pages
 
 touch .
 
