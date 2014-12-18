ef5a4942be55ecd0119621052956c74c8c42beb4
mdinger <mdinger.bugzilla@gmail.com>
2014-12-17 22:13:36 -0500

    Manually grab `ace` to use latest gitbook

--- Makefile
+++ Makefile
@@ -45,4 +45,4 @@ serve: node_modules/gitbook
 	$(GITBOOK) serve stage
 
 node_modules/gitbook:
-	npm install gitbook@0.7.1
+	npm install gitbook
--- setup-stage.sh
+++ setup-stage.sh
@@ -1,4 +1,11 @@
+#!/bin/dash
+
+ace_repository='https://github.com/ajaxorg/ace-builds/trunk/src-min-noconflict'
+ace_local_folder='node_modules/gitbook-plugin-rust-playpen/book/ace'
+
 mkdir -p bin
 mkdir -p stage/node_modules
+svn checkout ${ace_repository} ${ace_local_folder}
+
 ln -sf ../book.json stage
 ln -sf ../examples/README.md stage
