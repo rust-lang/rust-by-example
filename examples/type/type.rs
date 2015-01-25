3112fd846b4036e2ff309ec1c1544d7ce9c10000
Ophir LOJKINE <pere.jobs@gmail.com>
2015-01-25 17:50:25 +0100

    i suffix -> i32

--- examples/type/type.rs
+++ examples/type/type.rs
@@ -3,7 +3,7 @@ fn main() {
     let a_float: f64 = 1.0;
 
     // This variable is an `int`
-    let mut an_integer = 5i;
+    let mut an_integer = 5i32;
 
     // Error! The type of a variable can't be changed
     an_integer = true;
