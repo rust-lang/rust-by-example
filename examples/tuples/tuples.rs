069ebd36c0c8236b5d4b488e881cd054248fccd0
Steve Klabnik <steve@steveklabnik.com>
2015-02-25 13:07:36 -0500

    rephrase tuple indexing
    Fixes #333

--- examples/tuples/tuples.rs
+++ examples/tuples/tuples.rs
@@ -13,7 +13,7 @@ fn main() {
                       0.1f32, 0.2f64,
                       'a', true);
 
-    // Values can be extracted from the tuple using the `.N` syntax
+    // Values can be extracted from the tuple using tuple indexing
     println!("long tuple first value: {}", long_tuple.0);
     println!("long tuple second value: {}", long_tuple.1);
 
