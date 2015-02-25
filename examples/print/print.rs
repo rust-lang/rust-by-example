47351a15eacd07ac6a55a77822d20ef3587cbe7c
mdinger <mdinger.bugzilla@gmail.com>
2015-02-25 00:28:59 -0500

    {} to {:?}

--- examples/print/print.rs
+++ examples/print/print.rs
@@ -1,23 +1,23 @@
 fn main() {
-    // `{}` are placeholders for arguments that will be stringified.
-    println!("{} days", 31);
+    // `{:?}` are placeholders for arguments that will be stringified.
+    println!("{:?} days", 31);
 
     // Without a suffix, 31 becomes an i32. You can change what type 31 is,
     // with a suffix, and that is covered in the next chapter.
 
     // The positional arguments can be reused along the template.
-    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    println!("{0:?}, this is {1:?}. {1:?}, this is {0:?}", "Alice", "Bob");
 
     // Named arguments can also be used.
-    println!("{subject} {verb} {predicate}",
+    println!("{subject:?} {verb:?} {predicate:?}",
              predicate="over the lazy dog",
              subject="the quick brown fox",
              verb="jumps");
 
-    // Special formatting can be specified in the placeholder after a `:`.
-    println!("{} of {:b} people know binary, the other half don't", 1, 2);
+    // Special formatting can be specified in the place of the `?`.
+    println!("{:?} of {:b} people know binary, the other half don't", 1, 2);
 
     // Error! You are missing an argument.
-    println!("My name is {0}, {1} {0}", "Bond");
+    println!("My name is {0:?}, {1:?} {0:?}", "Bond");
     // FIXME ^ Add the missing argument: "James"
 }
