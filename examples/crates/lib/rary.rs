9e8af0cf3da7f2480f30daf936760909b2a1a880
Steve Klabnik <steve@steveklabnik.com>
2015-02-25 11:23:13 -0500

    liberty -> library

---
+++ examples/crates/lib/rary.rs
@@ -0,0 +1,13 @@
+pub fn public_function() {
+    println!("called rary's `public_function()`");
+}
+
+fn private_function() {
+    println!("called rary's `private_function()`");
+}
+
+pub fn indirect_access() {
+    print!("called rary's `indirect_access()`, that\n> ");
+
+    private_function();
+}
