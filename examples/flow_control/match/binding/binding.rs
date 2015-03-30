38c9ed15c3db3adc4b1f102e6925769237354c7a
mdinger <mdinger.bugzilla@gmail.com>
2015-03-30 18:39:10 -0400

    Clarify the purpose of match binding

--- examples/flow_control/match/binding/binding.rs
+++ examples/flow_control/match/binding/binding.rs
@@ -1,10 +1,16 @@
-fn main() {
-    let age = 3;
-    // TODO ^ Try different values for `age`
+// A function `age` which returns a `u32`.
+fn age() -> u32 {
+    15
+}
 
+fn main() {
     println!("Tell me type of person you are");
-    match age {
-        // Bind to `n` for the sequence of 1 through 12.
+
+    match age() {
+        0             => println!("I'm not born yet I guess"),
+        // Could `match` 1 ... 12 directly but then what age
+        // would the child be? Instead, bind to `n` for the
+        // sequence of 1 .. 12. Now the age can be reported.
         n @ 1  ... 12 => println!("I'm a child of age {:?}", n),
         n @ 13 ... 19 => println!("I'm a teen of age {:?}", n),
         // Nothing bound. Return the result.
