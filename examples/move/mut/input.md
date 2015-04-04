af5f2899baab535b2e32f087c943c34a9f4623f1
Bruno Beltran <brunobeltran0@gmail.com>
2015-04-04 13:49:28 -0400

    clarified semantics of immutable references in move/mut

--- examples/move/mut/input.md
+++ examples/move/mut/input.md
@@ -1,3 +1,6 @@
 Mutability of data can be changed when ownership is transferred.
 
+However, immutable data is protected from being accessed through
+mutable references at compile time.
+
 {mut.play}
--- examples/move/mut/mut.rs
+++ examples/move/mut/mut.rs
@@ -15,4 +15,32 @@ fn main() {
     *mutable_box = 4;
 
     println!("mutable_box now contains {}", mutable_box);
+
+    // on the other hand, suppose the variable itself is immutable
+    let x = &5u32;
+    let immutable_ref = &x;
+
+    println!("immutable_ref contains {}", immutable_ref);
+
+    // Mutability error
+    //*immutable_ref = 4;
+
+    // This is allowed, since the compiler can detect that "variable does not
+    // need to be mutable", but generates a warning that should be fixed.
+    let mut mutable_ref = immutable_ref;
+
+    println!("mutable_ref to immutable value still contains {}", mutable_ref);
+
+    // However, "const correctness" is enforced, and the following causes a
+    // mutability error at compile time!
+    //*mutable_ref += 1;
+
+    // the underlying value must be mutable for a mutable reference to exist
+    let mut x = 5u32;
+    let ref mut mutable_ref = x;
+    println!("mutable_ref to mutable value contains {}", mutable_ref);
+
+    // so this works
+    *mutable_ref = 4;
+    println!("mutable_ref to mutable value contains {}", mutable_ref);
 }
