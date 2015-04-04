c05ae77210692d0396750460407bd83b379e56b2
Bruno Beltran <brunobeltran0@gmail.com>
2015-04-04 16:31:27 -0400

    type/cast: returned old example to unrunnable state

--- examples/type/cast/cast.rs
+++ examples/type/cast/cast.rs
@@ -2,7 +2,7 @@ fn main() {
     let decimal = 65.4321_f32;
 
     // Error! No implicit conversion
-    //let integer: u8 = decimal;
+    let integer: u8 = decimal;
     // FIXME ^ Comment out this line
 
     // Explicit conversion
