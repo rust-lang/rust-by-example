898d886ce96c8148686305b8a2b30ff858bf4d2f
Bruno Beltran <brunobeltran0@gmail.com>
2015-04-04 16:27:59 -0400

    type/cast: put branch in correct state for pull request

--- examples/move/mut/mut.rs
+++ examples/move/mut/mut.rs
@@ -15,21 +15,4 @@ fn main() {
     *mutable_box = 4;
 
     println!("mutable_box now contains {}", mutable_box);
-
-    // on the other hand, once a reference is made immutable
-    // it cannot be borrowed mutably.
-    let mut x = 5u32;
-    let immutable_ref = &x;
-    // Compiler error!
-    let mutable_ref = &mut *immutable_ref;
-
-    // the underlying mutable value must be borrowed directly for a mutable
-    // reference to exist
-    let mut x = 5u32;
-    let mutable_ref = &mut x;
-    println!("mutable_ref to mutable value contains {}", mutable_ref);
-
-    // so this works
-    *mutable_ref = 4;
-    println!("mutable_ref to mutable value contains {}", mutable_ref);
 }
