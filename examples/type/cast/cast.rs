bca0dcc166680181f4dc646cf6a44119ab688fa8
Bruno Beltran <brunobeltran0@gmail.com>
2015-04-05 00:09:20 -0400

    type/cast: added detailed integral type casting rules

--- examples/type/cast/cast.rs
+++ examples/type/cast/cast.rs
@@ -10,4 +10,36 @@ fn main() {
     let character = integer as char;
 
     println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+    // when casting any value to an unsigned type, T, 
+    // std::T::MAX + 1 is added or subtracted until the value
+    // fits into the new type
+
+    // 1000 already fits in a u16
+    println!("1000 as a u16 is: {}", 1000 as u16);
+
+    // 1000 - 256 - 256 = 232
+    println!("1000 as a u8 is : {}", 1000 as u8);
+    // -1 + 256 = 255
+    println!("  -1 as a u8 is : {}", (-1i8) as u8);
+
+    // For positive numbers, this is the same as the modulus
+    println!("1000 mod 256 is : {}", 1000 % 256);
+
+    // When casting to a signed type, the result is the same as 
+    // first casting to the corresponding unsigned type then 
+    // taking the two's complement.
+
+    // Unless it already fits, of course.
+    println!(" 128 as a i16 is: {}", 128 as i16);
+    // 128 as u8 -> 128, whose two's complement in eight bits is:
+    println!(" 128 as a i8 is : {}", 128 as i8);
+
+    // repeating the example above
+    // 1000 as u8 -> 232
+    println!("1000 as a i8 is : {}", 1000 as i8);
+    // and the two's complement of 232 is -24
+    println!(" 232 as a i8 is : {}", 232 as i8);
+
+
 }
