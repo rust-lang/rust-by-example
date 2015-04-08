87e7f071a6eb924dcec96cfdc31cec7e6badef8d
woxtu <woxtup@gmail.com>
2015-04-08 23:27:49 +0900

    file: fix some comments

--- examples/file/create/create.rs
+++ examples/file/create/create.rs
@@ -16,7 +16,7 @@ fn main() {
     let path = Path::new("out/lorem_ipsum.txt");
     let display = path.display();
 
-    // Open a file in write-only mode, returns `IoResult<File>`
+    // Open a file in write-only mode, returns `io::Result<File>`
     let mut file = match File::create(&path) {
         Err(why) => panic!("couldn't create {}: {}",
                            display,
@@ -24,7 +24,7 @@ fn main() {
         Ok(file) => file,
     };
 
-    // Write the `LOREM_IPSUM` string to `file`, returns `IoResult<()>`
+    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
     match file.write_all(LOREM_IPSUM.as_bytes()) {
         Err(why) => {
             panic!("couldn't write to {}: {}", display,
--- examples/file/open/open.rs
+++ examples/file/open/open.rs
@@ -8,15 +8,15 @@ fn main() {
     let path = Path::new("hello.txt");
     let display = path.display();
 
-    // Open the path in read-only mode, returns `IoResult<File>`
+    // Open the path in read-only mode, returns `io::Result<File>`
     let mut file = match File::open(&path) {
-        // The `desc` field of `IoError` is a string that describes the error
+        // The `description` method of `io::Error` returns a string that describes the error
         Err(why) => panic!("couldn't open {}: {}", display,
                                                    Error::description(&why)),
         Ok(file) => file,
     };
 
-    // Read the file contents into a string, returns `IoResult<String>`
+    // Read the file contents into a string, returns `io::Result<usize>`
     let mut s = String::new();
     match file.read_to_string(&mut s) {
         Err(why) => panic!("couldn't read {}: {}", display,
