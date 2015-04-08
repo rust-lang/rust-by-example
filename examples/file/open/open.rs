cccd92621467141097d80f742186467776af6c4a
woxtu <woxtup@gmail.com>
2015-04-09 01:28:34 +0900

    file/open: add a newline

--- examples/file/open/open.rs
+++ examples/file/open/open.rs
@@ -10,7 +10,8 @@ fn main() {
 
     // Open the path in read-only mode, returns `io::Result<File>`
     let mut file = match File::open(&path) {
-        // The `description` method of `io::Error` returns a string that describes the error
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
         Err(why) => panic!("couldn't open {}: {}", display,
                                                    Error::description(&why)),
         Ok(file) => file,
