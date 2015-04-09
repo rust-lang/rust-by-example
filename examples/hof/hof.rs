5d19ff02764ef848a01ff8bb33f759f03373ff9b
mdinger <mdinger.bugzilla@gmail.com>
2015-04-09 15:13:45 -0400

    Fix breakage from 97f24a85965. Make hof cleaner.

--- examples/hof/hof.rs
+++ examples/hof/hof.rs
@@ -1,18 +1,14 @@
-#![feature(step_by)]
 #![feature(core)]
-
-// The `AdditiveIterator` trait adds the `sum` method to iterators
-use std::iter::AdditiveIterator;
 
 fn main() {
     println!("Find the sum of all the squared odd numbers under 1000");
-    let upper = 1000u32;
+    let upper = 1000;
 
     // Imperative approach
     // Declare accumulator variable
     let mut acc = 0;
     // Iterate: 0, 1, 2, ... to infinity
-    for n in (0u32..).step_by(1) {
+    for n in 0.. {
         // Square the number
         let n_squared = n * n;
 
@@ -27,17 +23,11 @@ fn main() {
     println!("imperative style: {}", acc);
 
     // Functional approach
-    let sum_of_squared_odd_numbers =
-        // All natural numbers
-        (0u32..).step_by(1).
-        // Squared
-        map(|n| n * n).
-        // Below upper limit
-        take_while(|&n| n < upper).
-        // That are odd
-        filter(|n| is_odd(*n)).
-        // Sum them
-        sum();
+    let sum_of_squared_odd_numbers: u32 =
+        (0..).map(|n| n * n)             // All natural numbers squared
+             .take_while(|&n| n < upper) // Below upper limit
+             .filter(|n| is_odd(*n))     // That are odd
+             .sum();                     // Sum them
     println!("functional style: {}", sum_of_squared_odd_numbers);
 }
 
