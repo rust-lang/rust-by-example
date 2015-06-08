6986e2872ccdf9adc9764d53b71df6689ed73995
Letheed <dev@daweb.se>
2015-06-08 23:18:53 +0200

    generics/phantom/testcase_units: mostly punctiation

--- examples/generics/phantom/testcase_units/units.rs
+++ examples/generics/phantom/testcase_units/units.rs
@@ -1,15 +1,15 @@
 use std::ops::Add;
 use std::marker::PhantomData;
 
-/// Null enumerations to define unit types
+/// Null enumerations define unit types.
 #[derive(Debug, Clone, Copy)]
 struct Inch;
 #[derive(Debug, Clone, Copy)]
 struct Mm;
 
-/// `Length` is phantom type with hidden parameter `Unit`
+/// `Length` is phantom type with hidden parameter `Unit`.
 ///
-/// `f64` already implements the `Clone` and `Copy` traits
+/// `f64` already implements the `Clone` and `Copy` traits.
 #[derive(Debug, Clone, Copy)]
 struct Length<Unit>(f64,PhantomData<Unit>);
 
@@ -19,30 +19,30 @@ impl<Unit> Add for Length<Unit> {
 
     // add() returns a new `Length` struct containing the sum.
     fn add(self, rhs: Length<Unit>) -> Length<Unit> {
-        // `+` calls the `Add` implementation for `f64`
+        // `+` calls the `Add` implementation for `f64`.
         Length(self.0 + rhs.0, PhantomData)
     }
 }
 
 fn main() {
-    // Specialize `one_foot` to have hidden parameter `Inch`
+    // Specializes `one_foot` to have hidden parameter `Inch`.
     let one_foot:  Length<Inch> = Length(12.0, PhantomData);
-    // `one_meter` has hidden parameter `Mm`
+    // `one_meter` has hidden parameter `Mm`.
     let one_meter: Length<Mm>   = Length(1000.0, PhantomData);
 
-    // `+` calls the `add()` method we implemented for `Length<Unit>`
+    // `+` calls the `add()` method we implemented for `Length<Unit>`.
     //
     // Since `Length` implements `Clone` + `Copy`, `add()` does not consume
-    // `one_foot` and `one_meter` but makes a copy of them in `self` and `rhs`
+    // `one_foot` and `one_meter` but makes a copy of them in `self` and `rhs`.
     let two_feet = one_foot + one_foot;
     let two_meters = one_meter + one_meter;
 
-    // Addition works
+    // Addition works.
     println!("one foot + one_foot = {:?}", two_feet);
     println!("one meter + one_meter = {:?}", two_meters);
 
-    // Nonsensical operations fail as they should
-    // Error: type mismatch
+    // Nonsensical operations fail as they should:
+    // Error: type mismatch.
     //let one_feter = one_foot + one_meter;
 }
 
