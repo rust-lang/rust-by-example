6c80d60c645ad1b9fad3c186b9fecf918d752c2b
mdinger <mdinger.bugzilla@gmail.com>
2015-06-26 12:53:59 -0400

    Move `use` into main

--- examples/custom_types/enum/enum_use/use.rs
+++ examples/custom_types/enum/enum_use/use.rs
@@ -1,13 +1,6 @@
 // An attribute to hide warnings for unused code.
 #![allow(dead_code)]
 
-// Explicitly `use` each name so they are available without
-// manual scoping.
-use Status::{Poor, Rich};
-
-// Automatically `use` each name inside `Work`.
-use Work::*;
-
 enum Status {
     Rich,
     Poor,
@@ -19,17 +12,25 @@ enum Work {
 }
 
 fn main() {
+    // Explicitly `use` each name so they are available without
+    // manual scoping.
+    use Status::{Poor, Rich};
+    // Automatically `use` each name inside `Work`.
+    use Work::*;
+
     // Equivalent to `Status::Poor`.
     let status = Poor;
     // Equivalent to `Work::Civilian`.
     let work = Civilian;
 
     match status {
+        // Note the lack of scoping because of the explicit `use` above.
         Rich => println!("The rich have lots of money!"),
         Poor => println!("The poor have no money..."),
     }
 
     match work {
+        // Note again the lack of scoping.
         Civilian => println!("Civilians work!"),
         Soldier  => println!("Soldiers fight!"),
     }
