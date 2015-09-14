50a53c13183fe54c51a43a17451b56af83585f8e
Junhua <zhjh1209@gmail.com>
2015-09-14 14:58:34 +0800

    Update unit_test.rs
    Correct the error of calculating the distance between two points.

--- examples/meta/test/unit_test.rs
+++ examples/meta/test/unit_test.rs
@@ -10,8 +10,8 @@ mod test {
     // A helper function `distance_test` will need.
     fn distance(a: (f32, f32), b: (f32, f32)) -> f32 {
         (
-            (b.0.powi(2) - a.0.powi(2)) +
-            (b.1.powi(2) - a.1.powi(2))
+            (b.0 - a.0).powi(2) +
+            (b.1 - a.1).powi(2)
         ).sqrt()
     }
 
