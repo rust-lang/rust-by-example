3512f459f3cc83eb8eb89cf1f1f03334dcebc13f
mdinger <mdinger.bugzilla@gmail.com>
2015-09-30 19:52:39 -0400

    Revert "Use local highlighting file until ace-builds updates again"
    This reverts commit af64ffd0c6146ddcd9a1fdc5b9f156ed918716b8.

--- setup-stage.sh
+++ setup-stage.sh
@@ -6,8 +6,7 @@ ace_playpen_local='node_modules/gitbook-plugin-rust-playpen'
 mkdir -p bin
 mkdir -p stage/node_modules
 svn checkout ${ace_repository} ${ace_playpen_local}'/book/ace'
-# cp ${ace_playpen_local}'/book/ace/mode-rust.js' ${ace_playpen_local}'/book/mode-rust.js'
-cp ${ace_playpen_local}'/book/mode-rust.js.bak' ${ace_playpen_local}'/book/mode-rust.js'
+cp ${ace_playpen_local}'/book/ace/mode-rust.js' ${ace_playpen_local}'/book/mode-rust.js'
 
 ln -sf ../book.json stage
 ln -sf ../examples/README.md stage
