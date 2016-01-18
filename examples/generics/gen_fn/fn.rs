7a7a53bbe126b42ae214d0ba13bcea28ad9ae608
Xmasreturns <j.l@utexas.edu>
2016-01-18 11:38:42 -0800

    Wording change
    non-generic -> not generic

--- examples/generics/gen_fn/fn.rs
+++ examples/generics/gen_fn/fn.rs
@@ -6,7 +6,7 @@ struct SGen<T>(T); // Generic type `SGen`.
 // them and immediately go out of scope freeing the variable.
 
 // Define a function `reg_fn` that takes an argument `s` of type `S`.
-// This has no `<T>` so this must be a non-generic function.
+// This has no `<T>` so this is not a generic function.
 fn reg_fn(s: S) {}
 
 // Define a function `gen_spec_t` that takes an argument `s` of type `SGen<T>`
@@ -16,7 +16,7 @@ fn gen_spec_t(s: SGen<A>) {}
 
 // Define a function `gen_spec_i32` that takes an argument `s` of type `SGen<i32>`
 // that has been explicitly given the type parameter `i32`.
-// This function is similarly non-generic.
+// This function is also not generic.
 fn gen_spec_i32(s: SGen<i32>) {}
 
 // Define a function `generic` that takes an argument `s` of type `SGen<T>`.
