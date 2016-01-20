f4c3f3aa9399de3fa3ff92f5d243573f7668306f
Xmasreturns <j.l@utexas.edu>
2016-01-19 16:59:25 -0800

    Update Ch13 - Phantom types
    Made mention of phantom types more consistent by referring to the type
    parameters themselves as phantom type parameters. Also includes small
    updates to testcase example

--- examples/generics/phantom/testcase_units/units.rs
+++ examples/generics/phantom/testcase_units/units.rs
@@ -1,13 +1,13 @@
 use std::ops::Add;
 use std::marker::PhantomData;
 
-/// Null enumerations define unit types.
+/// Create void enumerations to define unit types.
 #[derive(Debug, Clone, Copy)]
-struct Inch;
+enum Inch {}
 #[derive(Debug, Clone, Copy)]
-struct Mm;
+enum Mm {}
 
-/// `Length` is phantom type with hidden parameter `Unit`.
+/// `Length` is a type with phantom type parameter `Unit`.
 ///
 /// `f64` already implements the `Clone` and `Copy` traits.
 #[derive(Debug, Clone, Copy)]
@@ -25,15 +25,15 @@ impl<Unit> Add for Length<Unit> {
 }
 
 fn main() {
-    // Specializes `one_foot` to have hidden parameter `Inch`.
+    // Specifies `one_foot` to have phantom type parameter `Inch`.
     let one_foot:  Length<Inch> = Length(12.0, PhantomData);
-    // `one_meter` has hidden parameter `Mm`.
+    // `one_meter` has phantom type parameter `Mm`.
     let one_meter: Length<Mm>   = Length(1000.0, PhantomData);
 
     // `+` calls the `add()` method we implemented for `Length<Unit>`.
     //
-    // Since `Length` implements `Clone` + `Copy`, `add()` does not consume
-    // `one_foot` and `one_meter` but makes a copy of them in `self` and `rhs`.
+    // Since `Length` implements `Copy`, `add()` does not consume
+    // `one_foot` and `one_meter` but copies them into `self` and `rhs`.
     let two_feet = one_foot + one_foot;
     let two_meters = one_meter + one_meter;
 
@@ -42,7 +42,7 @@ fn main() {
     println!("one meter + one_meter = {:?}", two_meters);
 
     // Nonsensical operations fail as they should:
-    // Error: type mismatch.
+    // Compile-time Error: type mismatch.
     //let one_feter = one_foot + one_meter;
 }
 
