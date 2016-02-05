41d3ea57a28b6a163ca964318ecdb1fe4f223ad9
Xmasreturns <j.l@utexas.edu>
2016-02-05 14:07:41 -0800

    Updated 14.4
    Reworded description to incorporate language from PR #655. Minor changes
    to example for clarity

--- examples/scope/lifetime/input.md
+++ examples/scope/lifetime/input.md
@@ -1,11 +1,15 @@
 A *lifetime* is a construct the compiler (also called the borrow checker)
-uses to ensure all borrows are valid. Specifically, a lifetime starts when
-a variable is created and ends when it is destroyed. It can be visualized as
-vertical distance but since size is only relevant for subsets and supersets,
-scope is considered the more appropriate descriptive term.
+uses to ensure all borrows are valid. Specifically, a variable's lifetime 
+begins when it is created and ends when it is destroyed. While lifetimes 
+and scopes are often referred to together, they are not the same. 
 
-Borrowing (via `&` for example) creates new lifetimes. A borrow is valid
-as long as the borrow ends before (inside) the lender is destroyed.
+Take, for example, the case where we borrow a variable via `&`. The 
+borrow has a lifetime that is determined by where it is declared. As a result, 
+the borrow is valid as long as it ends before the lender is destroyed. However, 
+the scope of the borrow is determined by where the reference is used.
+
+In the following example and in the rest of this section, we will see how 
+lifetimes relate to scopes, as well as how the two differ.
 
 {lifetime.play}
 
--- examples/scope/lifetime/lifetime.rs
+++ examples/scope/lifetime/lifetime.rs
@@ -1,22 +1,22 @@
-// Here, lifetimes are annotated with lines denoting when each variable
-// is created and destroyed. `i` has the largest lifetime because its
-// scope entirely encloses both `borrow1` and `borrow2`. The "size"
-// of `borrow1` compared with `borrow2` is irrelevant since they are
-// disjoint.
+// Lifetimes are annotated below with lines denoting the creation
+// and destruction of each variable.
+// `i` has the longest lifetime because its scope entirely encloses 
+// both `borrow1` and `borrow2`. The duration of `borrow1` compared 
+// to `borrow2` is irrelevant since they are disjoint.
 fn main() {
-    let i = 3; // Lifetime for `i` starts. ───────┐
-    //                                            │
-    { //                                          │
-        let borrow1 = &i; // `borrow1` starts. ──┐│
-        //                                       ││
-        println!("borrow1: {}", borrow1); //     ││
-    } // `borrow1 ends. ─────────────────────────┘│
-    //                                            │
-    //                                            │
-    { //                                          │
-        let borrow2 = &i; // `borrow2` starts. ──┐│
-        //                                       ││
-        println!("borrow2: {}", borrow2); //     ││
-    } // `borrow2` ends. ────────────────────────┘│
-    //                                            │
-}   // Lifetime ends. ────────────────────────────┘
+    let i = 3; // Lifetime for `i` starts. ────────────────┐
+    //                                                     │
+    { //                                                   │
+        let borrow1 = &i; // `borrow1` lifetime starts. ──┐│
+        //                                                ││
+        println!("borrow1: {}", borrow1); //              ││
+    } // `borrow1 ends. ──────────────────────────────────┘│
+    //                                                     │
+    //                                                     │
+    { //                                                   │
+        let borrow2 = &i; // `borrow2` lifetime starts. ──┐│
+        //                                                ││
+        println!("borrow2: {}", borrow2); //              ││
+    } // `borrow2` ends. ─────────────────────────────────┘│
+    //                                                     │
+}   // Lifetime ends. ─────────────────────────────────────┘
