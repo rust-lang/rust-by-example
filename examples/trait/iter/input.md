126912df89519e93ef7b7ac80d498ef563f0c467
Xmasreturns <j.l@utexas.edu>
2016-04-01 21:14:06 -0700

    Readability and grammar changes
    Shifted some information from .rs to .md to bring out some high points.
    Reworded some example code for readability.

--- examples/trait/iter/input.md
+++ examples/trait/iter/input.md
@@ -1,9 +1,16 @@
-The `Iterator` trait is used to implement iterators over collections (like
-arrays) and lazy value generators.
+The `Iterator` trait is used to implement iterators over collections such as arrays.
+
+The trait requires only a method to be defined for the `next` element, 
+which may be manually defined in an `impl` block or automatically 
+defined (as in arrays and ranges).
+
+As a point of convenience for common situations, the `for` construct 
+turns some collections into iterators using the [`.into_iterator()`][intoiter] method.
+
+Methods that can be accessed using the `Iterator` trait in addition 
+to those shown in the example below can be found [here][iter].
 
 {iter.play}
 
-The `Iterator` trait gives access to
-[several methods][iter].
-
+[intoiter]: https://doc.rust-lang.org/std/iter/trait.IntoIterator.html
 [iter]: http://doc.rust-lang.org/core/iter/trait.Iterator.html
--- examples/trait/iter/iter.rs
+++ examples/trait/iter/iter.rs
@@ -3,30 +3,34 @@ struct Fibonacci {
     next: u32,
 }
 
-// Implement 'Iterator' for 'Fibonacci'
+// Implement `Iterator` for `Fibonacci`.
+// The `Iterator` trait only requires a method to be defined for the `next` element.
 impl Iterator for Fibonacci {
     type Item = u32;
-    // The 'Iterator' trait only requires the 'next' method to be defined. The
-    // return type is 'Option<T>', 'None' is returned when the 'Iterator' is
-    // over, otherwise the next value is returned wrapped in 'Some'
+    
+    // Here, we define the sequence using `.curr` and `.next`.
+    // The return type is `Option<T>`:
+    //     * When the `Iterator` is finished, `None` is returned.
+    //     * Otherwise, the next value is wrapped in `Some` and returned.
     fn next(&mut self) -> Option<u32> {
         let new_next = self.curr + self.next;
 
         self.curr = self.next;
         self.next = new_next;
 
-        // 'Some' is always returned, this is an infinite value generator
+        // Since there's no endpoint to a Fibonacci sequence, the `Iterator` 
+        // will never return `None`, and `Some` is always returned.
         Some(self.curr)
     }
 }
 
-// Returns a fibonacci sequence generator
+// Returns a Fibonacci sequence generator
 fn fibonacci() -> Fibonacci {
     Fibonacci { curr: 1, next: 1 }
 }
 
 fn main() {
-    // Iterator that generates: 0, 1 and 2
+    // `0..3` is an `Iterator` that generates: 0, 1, and 2.
     let mut sequence = 0..3;
 
     println!("Four consecutive `next` calls on 0..3");
@@ -35,22 +39,20 @@ fn main() {
     println!("> {:?}", sequence.next());
     println!("> {:?}", sequence.next());
 
-    // The for construct will iterate an 'Iterator' until it returns 'None'.
-    // Every 'Some' value is unwrapped and bound to a variable.
-    println!("Iterate over 0..3 using for");
+    // `for` works through an `Iterator` until it returns `None`.
+    // Each `Some` value is unwrapped and bound to a variable (here, `i`).
+    println!("Iterate through 0..3 using `for`");
     for i in 0..3 {
         println!("> {}", i);
     }
 
-    // The 'take(n)' method will reduce an iterator to its first 'n' terms,
-    // which is pretty useful for infinite value generators
+    // The `take(n)` method reduces an `Iterator` to its first `n` terms.
     println!("The first four terms of the Fibonacci sequence are: ");
     for i in fibonacci().take(4) {
         println!("> {}", i);
     }
 
-    // The 'skip(n)' method will shorten an iterator by dropping its first 'n'
-    // terms
+    // The `skip(n)` method shortens an `Iterator` by dropping its first `n` terms.
     println!("The next four terms of the Fibonacci sequence are: ");
     for i in fibonacci().skip(4).take(4) {
         println!("> {}", i);
@@ -58,7 +60,7 @@ fn main() {
 
     let array = [1u32, 3, 3, 7];
 
-    // The 'iter' method produces an 'Iterator' over an array/slice
+    // The `iter` method produces an `Iterator` over an array/slice.
     println!("Iterate the following array {:?}", &array);
     for i in array.iter() {
         println!("> {}", i);
