e9fa69b54fcc1f36588743da6b6dced7fadbb9e1
Xmasreturns <j.l@utexas.edu>
2016-04-26 19:23:12 -0700

    Updated Ch16.4
    Minor grammar and punctuation fixes

--- examples/error/result/result.rs
+++ examples/error/result/result.rs
@@ -1,7 +1,7 @@
 fn double_number(number_str: &str) -> i32 {
-    // It might not always be possible to parse a string into the other type
+    // It might not always be possible to parse a string into the other type,
     // so `parse()` returns a `Result` indicating possible failure. Let's
-    // just try `unwrap()` to get the number out. Will it bite us?
+    // try using `unwrap()` to get the number out. Will it bite us?
     2 * number_str.parse::<i32>().unwrap()
 }
 
