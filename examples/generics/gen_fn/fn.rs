10c6476bb0e4915c9397a38f5d22a0058e544002
Xmasreturns <j.l@utexas.edu>
2016-04-26 19:12:51 -0700

    Updated Ch13.1
    Fix punctuation and minor grammar changes

--- examples/generics/gen_fn/fn.rs
+++ examples/generics/gen_fn/fn.rs
@@ -2,24 +2,24 @@ struct A;          // Concrete type `A`.
 struct S(A);       // Concrete type `S`.
 struct SGen<T>(T); // Generic type `SGen`.
 
-// These functions all take ownership of the variable passed into
-// them and immediately go out of scope freeing the variable.
+// The following functions all take ownership of the variable passed into
+// them and immediately go out of scope, freeing the variable.
 
-// Define a function `reg_fn` that takes an argument `s` of type `S`.
+// Define a function `reg_fn` that takes an argument `_s` of type `S`.
 // This has no `<T>` so this is not a generic function.
 fn reg_fn(_s: S) {}
 
-// Define a function `gen_spec_t` that takes an argument `s` of type `SGen<T>`
-// that has been explicitly given the type parameter `A`. Because A has not 
-// been specified as a generic type parameter for gen_spec_t, it is not generic.
+// Define a function `gen_spec_t` that takes an argument `_s` of type `SGen<T>`.
+// It has been explicitly given the type parameter `A`, but because `A` has not 
+// been specified as a generic type parameter for `gen_spec_t`, it is not generic.
 fn gen_spec_t(_s: SGen<A>) {}
 
-// Define a function `gen_spec_i32` that takes an argument `s` of type `SGen<i32>`
-// that has been explicitly given the type parameter `i32`.
-// This function is also not generic.
+// Define a function `gen_spec_i32` that takes an argument `_s` of type `SGen<i32>`.
+// It has been explicitly given the type parameter `i32`, which is a specific type.
+// Because `i32` is not a generic type, this function is also not generic.
 fn gen_spec_i32(_s: SGen<i32>) {}
 
-// Define a function `generic` that takes an argument `s` of type `SGen<T>`.
+// Define a function `generic` that takes an argument `_s` of type `SGen<T>`.
 // Because `SGen<T>` is preceded by `<T>`, this function is generic over `T`.
 fn generic<T>(_s: SGen<T>) {}
 
