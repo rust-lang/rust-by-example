659c6cf5563923a8500744f40304f3725994ec72
Xmasreturns <j.l@utexas.edu>
2016-05-03 17:27:38 -0700

    Updated and restructured Ch16.5, 16.6
    Readability and grammar changes
    Renamed "Result as an alias" to "aliases for Result"
    Made "map for Result" and "aliases for Result" into children of "Result"

--- examples/error/result/result_map/result.rs
+++ examples/error/result/result_map/result.rs
@@ -1,8 +1,7 @@
 use std::num::ParseIntError;
 
 // With the return type rewritten, we proceed to use pattern matching without
-// `unwrap()` but it is tedious. Couldn't a combinator like in the `Option`
-// example also be used here? Yes.
+// `unwrap()`, but it is tedious.
 fn double_number(number_str: &str) -> Result<i32, ParseIntError> {
     match number_str.parse::<i32>() {
         Ok(n)  => Ok(2 * n),
@@ -10,8 +9,9 @@ fn double_number(number_str: &str) -> Result<i32, ParseIntError> {
     }
 }
 
-// The exact same but written with `map()`. Modify if the value is valid,
-// otherwise pass the error on.
+// As with `Option`, we can use combinators such as `map()`.
+// This function is otherwise identical to the one above and reads:
+// Modify n if the value is valid, otherwise pass on the error.
 fn double_number_map(number_str: &str) -> Result<i32, ParseIntError> {
     number_str.parse::<i32>().map(|n| 2 * n)
 }
@@ -24,11 +24,11 @@ fn print(result: Result<i32, ParseIntError>) {
 }
 
 fn main() {
-    // Still presents a reasonable answer.
+    // This still presents a reasonable answer.
     let twenty = double_number("10");
     print(twenty);
 
-    // This is now much better than before with the messy `panic`.
+    // The following now provides a much more useful error message
     let tt = double_number_map("t");
     print(tt);
 }
