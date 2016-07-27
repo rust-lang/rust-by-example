7efb27cd432271acd2580038b28bec790e3a61bc
Igor Shaposhnik <bulbazoron@gmail.com>
2016-07-27 08:47:26 +0300

    Перевод Primitives.Literals and operators

--- examples/primitives/literals/literals.rs
+++ examples/primitives/literals/literals.rs
@@ -1,23 +1,24 @@
 fn main() {
-    // Integer addition
+    // Целочисленное сложение
     println!("1 + 2 = {}", 1u32 + 2);
 
-    // Integer subtraction
+    // Целочисленное вычитание
     println!("1 - 2 = {}", 1i32 - 2);
-    // TODO ^ Try changing `1i32` to `1u32` to see why the type is important
+    // TODO ^ Попробуйте изменить `1i32` на `1u32`
+    // чтобы убедится насколько важен тип данных
 
-    // Short-circuiting boolean logic
+    // Булева логика
     println!("true AND false is {}", true && false);
     println!("true OR false is {}", true || false);
     println!("NOT true is {}", !true);
 
-    // Bitwise operations
+    // Побитовые операции
     println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
     println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
     println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
     println!("1 << 5 is {}", 1u32 << 5);
     println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
 
-    // Use underscores to improve readability!
+    // Использование подчеркивания для улучшения читаемости!
     println!("One million is written as {}", 1_000_000u32);
 }
