169cc968053777764b99daea5d3310ca0b25fb53
ubsan <npmazzuca@gmail.com>
2016-07-26 18:04:22 -0700

    Fix mutability issues

--- examples/std_misc/process/wait/wait.rs
+++ examples/std_misc/process/wait/wait.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 
 fn main() {
-    let child = Command::new("sleep").arg("5").spawn().unwrap();
+    let mut child = Command::new("sleep").arg("5").spawn().unwrap();
     let _result = child.wait().unwrap();
 
     println!("reached end of main");
