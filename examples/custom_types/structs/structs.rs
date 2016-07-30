f99e33572a88addf4295f2e928f479b2e5ee4d57
Igor Shaposhnik <bulbazoron@gmail.com>
2016-07-31 00:31:00 +0300

    Перевод Custom types.Structures

--- examples/custom_types/structs/structs.rs
+++ examples/custom_types/structs/structs.rs
@@ -1,16 +1,16 @@
-// A unit struct
+// Единичная структура
 struct Nil;
 
-// A tuple struct
+// Кортежная структура
 struct Pair(i32, f64);
 
-// A struct with two fields
+// Структура с двумя полями
 struct Point {
     x: f64,
     y: f64,
 }
 
-// Structs can be reused as fields of another struct
+// Структуры могут быть использованы как поля другой структуры
 #[allow(dead_code)]
 struct Rectangle {
     p1: Point,
@@ -18,28 +18,28 @@ struct Rectangle {
 }
 
 fn main() {
-    // Instantiate a `Point`
+    // Создаем связь со структурой `Point`
     let point: Point = Point { x: 0.3, y: 0.4 };
 
-    // Access the fields of the point
+    // Получаем доступ к полям структуры `point`
     println!("point coordinates: ({}, {})", point.x, point.y);
 
-    // Destructure the point using a `let` binding
+    // Деструктурируем `point` создавая связь с помощью `let`
     let Point { x: my_x, y: my_y } = point;
 
     let _rectangle = Rectangle {
-        // struct instantiation is an expression too
+        // инициализация стуктуры так же является выражением
         p1: Point { x: my_y, y: my_x },
         p2: point,
     };
 
-    // Instantiate a unit struct
+    // Создаем связь с единичной структурой
     let _nil = Nil;
 
-    // Instantiate a tuple struct
+    // Создаем связь с кортежной структурой
     let pair = Pair(1, 0.1);
 
-    // Destructure a tuple struct
+    // Деструктурируем кортежную структуру
     let Pair(integer, decimal) = pair;
 
     println!("pair contains {:?} and {:?}", integer, decimal);
