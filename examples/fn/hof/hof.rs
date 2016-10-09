82487892ffdb539f72bc34cb047a1d736db48024
Igor Shaposhnik <shaposhnikigor95@bk.ru>
2016-10-09 23:05:12 +0300

    Перевод Functions

--- examples/fn/hof/hof.rs
+++ examples/fn/hof/hof.rs
@@ -3,32 +3,32 @@ fn is_odd(n: u32) -> bool {
 }
 
 fn main() {
-    println!("Find the sum of all the squared odd numbers under 1000");
+    println!("Найти сумму всех квадратов нечетных числ не больше 1000");
     let upper = 1000;
 
-    // Imperative approach
-    // Declare accumulator variable
+    // Императивный подход
+    // Объявляем переменную накопитель
     let mut acc = 0;
-    // Iterate: 0, 1, 2, ... to infinity
+    // Итерировать: 0, 1, 2, ... до бесконечности
     for n in 0.. {
-        // Square the number
+        // Квадрат числа
         let n_squared = n * n;
 
         if n_squared >= upper {
-            // Break loop if exceeded the upper limit
+            // станавить цикл, если превысили верхний лимит
             break;
         } else if is_odd(n_squared) {
-            // Accumulate value, if it's odd
+            // Складывать число, если оно нечетное
             acc += n_squared;
         }
     }
     println!("imperative style: {}", acc);
 
-    // Functional approach
+    // Функциональный подход
     let sum_of_squared_odd_numbers: u32 =
-        (0..).map(|n| n * n)             // All natural numbers squared
-             .take_while(|&n| n < upper) // Below upper limit
-             .filter(|n| is_odd(*n))     // That are odd
-             .fold(0, |sum, i| sum + i); // Sum them
+        (0..).map(|n| n * n)             // Все натуральные числа в квадрате
+             .take_while(|&n| n < upper) // Ниже верхнего предела
+             .filter(|n| is_odd(*n))     // Это нечетные
+             .fold(0, |sum, i| sum + i); // Суммируем
     println!("functional style: {}", sum_of_squared_odd_numbers);
 }
