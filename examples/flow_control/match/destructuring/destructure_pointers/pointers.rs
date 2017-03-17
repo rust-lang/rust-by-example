ae221f2073762e62c94c3ca5146063ad3dcd922e
Ilya Druzhinin <ilya.druzhinin@gmail.com>
2017-03-18 00:23:56 +0400

    Перевод примера для 7.5.1.3 pointers/ref (#31)

--- examples/flow_control/match/destructuring/destructure_pointers/pointers.rs
+++ examples/flow_control/match/destructuring/destructure_pointers/pointers.rs
@@ -1,50 +1,50 @@
 fn main() {
-    // Assign a reference of type `i32`. The `&` signifies there
-    // is a reference being assigned.
+    // Присваиваем ссылку на тип `i32`. 
+    // Символ `&` означает, что присваивается ссылка.
     let reference = &4;
 
     match reference {
-        // If `reference`s is pattern matched against `&val`, it results
-        // in a comparison like:
+        // Если `reference` - это шаблон, который сопоставляется с `&val`,
+        // то это приведёт к сравнению:
         // `&i32`
         // `&val`
-        // ^ We see that if the matching `&`s are dropped, then the `i32`
-        // should be assigned to `val`.
-        &val => println!("Got a value via destructuring: {:?}", val),
+        // ^ Мы видим, что если отбросить сопоставляемые `&`, 
+        // то переменной `val` должно быть присвоено `i32`.
+        &val => println!("Получаем значение через деструктуризацию: {:?}", val),
     }
 
-    // To avoid the `&`, you dereference before matching.
+    // Чтобы избежать символа `&`, нужно разыменовывать ссылку до сопоставления.
     match *reference {
-        val => println!("Got a value via dereferencing: {:?}", val),
+        val => println!("Получаем значение через разыменование: {:?}", val),
     }
 
-    // What if you don't start with a reference? `reference` was a `&`
-    // because the right side was already a reference. This is not
-    // a reference because the right side is not one.
+    // Что если у нас нет ссылки? `reference` была с `&`,
+    // потому что правая часть была ссылкой. Но это не ссылка, 
+    // потому что правая часть ею не является.
     let _not_a_reference = 3;
 
-    // Rust provides `ref` for exactly this purpose. It modifies the
-    // assignment so that a reference is created for the element; this
-    // reference is assigned.
+    // Rust предоставляет ключевое слово `ref` именно для этой цели. 
+    // Оно изменяет присваивание так, что создаётся ссылка для элемента. 
+    // Теперь ссылка присвоена.
     let ref _is_a_reference = 3;
 
-    // Accordingly, by defining 2 values without references, references
-    // can be retrieved via `ref` and `ref mut`.
+    // Соответственно, для определения двух значений без ссылок, 
+    // ссылки можно назначить с помощью `ref` и `ref mut`.
     let value = 5;
     let mut mut_value = 6;
 
-    // Use `ref` keyword to create a reference.
+    // Используйте ключевое слово `ref` для создания ссылки.
     match value {
-        ref r => println!("Got a reference to a value: {:?}", r),
+        ref r => println!("Получили ссылку на значение: {:?}", r),
     }
 
-    // Use `ref mut` similarly.
+    // Используйте `ref mut` аналогичным образом.
     match mut_value {
         ref mut m => {
-            // Got a reference. Gotta dereference it before we can
-            // add anything to it.
+            // Получаем ссылку. Её нужно разыменовать, 
+            // прежде чем мы сможем что-то добавить.
             *m += 10;
-            println!("We added 10. `mut_value`: {:?}", m);
+            println!("Мы добавили 10. `mut_value`: {:?}", m);
         },
     }
 }
