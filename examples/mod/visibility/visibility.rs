91d25050508fa7411fdccd71aa49498fe8caf476
Taylor Cramer <cramertaylorj@gmail.com>
2017-03-18 00:05:08 -0700

    Add pub(self) and pub(super)

--- examples/mod/visibility/visibility.rs
+++ examples/mod/visibility/visibility.rs
@@ -29,15 +29,30 @@ mod my_mod {
         }
 
         // Functions declared using `pub(in path)` syntax are only visible
-        // within the given path
+        // within the given path. `path` must be a parent or ancestor module
         pub(in my_mod) fn public_function_in_my_mod() {
-        println!("called `my_mod::nested::public_function_in_my_mod()`")
-    }
+            print!("called `my_mod::nested::public_function_in_my_mod()`, that\n > ");
+            public_function_in_nested()
+        }
+
+        // Functions declared using `pub(self)` syntax are only visible within
+        // the current module
+        pub(self) fn public_function_in_nested() {
+            println!("called `my_mod::nested::public_function_in_nested");
+        }
+
+        // Functions declared using `pub(super)` syntax are only visible within
+        // the parent module
+        pub(super) fn public_function_in_super_mod() {
+            println!("called my_mod::nested::public_function_in_super_mod");
+        }
     }
 
     pub fn call_public_function_in_my_mod() {
         print!("called `my_mod::call_public_funcion_in_my_mod()`, that\n> ");
         nested::public_function_in_my_mod();
+        print!("> ");
+        nested::public_function_in_super_mod();
     }
 
     // pub(crate) makes functions visible only within the current crate
